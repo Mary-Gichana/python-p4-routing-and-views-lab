55fd8a815399c973e010db48bf0361d80417a297
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-13 14:06:15 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>/', strict_slashes=False)
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>/', strict_slashes=False)
+def count(parameter):
+    count = ''
+    for i in range(parameter):
+        count += str(i) + '\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>/', strict_slashes=False)
+
+
+def math(num1, operation, num2):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "div" or operation == "/":  
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error: Division by zero"
+    elif operation == "%":  
+        result = num1 % num2
+    else:
+        return "Error: Invalid operation"
+    
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
